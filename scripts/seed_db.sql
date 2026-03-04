d230de066b861c75a80bc80ffb0bbea37d2c95d7
Charuvarthan-T <charuvarthan05@gmail.com>
2026-03-04 15:34:14 +0530

    gemini model worksgit add .

--- scripts/seed_db.sql
+++ scripts/seed_db.sql
@@ -7,3 +7,12 @@ orders integer NOT NULL,
 created_at timestamptz NOT NULL DEFAULT now(),
 PRIMARY KEY (date, region, category)
 );
+
+
+INSERT INTO sales_daily VALUES
+('2025-09-01','North','Electronics',125000.50,310),
+('2025-09-01','South','Grocery',54000.00,820),
+('2025-09-01','West','Fashion',40500.00,190),
+('2025-09-02','North','Electronics',132500.00,332),
+('2025-09-02','West','Fashion',45500.00,210),
+('2025-09-02','East','Grocery',62000.00,870);
--- testllm.py
+++ testllm.py
@@ -1,9 +1,6 @@
 import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyCcJH6gMnE9scKHSTKfGafCzYErNKuHx9I")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel("gemini-2.5-flash")
 response = model.generate_content("Say hello world")
-
 print(response.text)
